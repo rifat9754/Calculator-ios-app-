3125d0bfab6291c0de1ac75696790c3daf1e3b0c
Tahmid <tahmid2007097@stud.kuet.ac.bd>
2024-11-12 23:10:45 +0600

    Initial commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ahanaf Tahmid Rifat on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
